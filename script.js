fff9f1d1a8d5b7f2f948e906f954f0531d6f0a8a
Ninja-Jutsu <ninja-jutsu-way@gmail.com>
2023-11-07 01:08:07 +0800

    Vanessa's Logic Works

--- script.js
+++ script.js
@@ -15,7 +15,6 @@
     token : "O"
 }
 
-
 function placeToken(player, position) {
     for(i = 0; i < board.length ; i++){
     let tokenPosition = board[i].findIndex(cell => cell === position)
@@ -26,9 +25,53 @@ function placeToken(player, position) {
         board[i][tokenPosition] = player.token
       }
       console.log(board)
-
-  }
+    }i
 }
 
-placeToken(player1 , 4)
+function winGame(){
+  let text = ''
+  for (i = 0; i < board.length ; i++){
+      for (j = 0; j < board.length; j++){
+        text = text + board[i][j]
+      }
+      if (text === "XXX"){
+        return console.log("Player1 Wins")
+      }
+      else if (text === "OOO"){
+        return console.log("Player2 Wins")
+      }
+    text = '';
+  }
+
+  for (i = 0 ; i < 3 ; i++){
+    for (j = 0; j < 3 ; j++){
+      text = text + board[j][i]
+    }
+    if (text === "XXX"){
+      return console.log("Player1 Wins")
+    }
+    else if (text === "OOO"){
+      return console.log("Player2 Wins")
+    }
+    text = '';
+    }
+}
+console.log(board)
 placeToken(player2 , 7)
+placeToken(player1 , 2)
+
+placeToken(player2 , 9)
+placeToken(player1 , 5)
+
+placeToken(player2 , 1)
+placeToken(player1 , 4)
+
+placeToken(player2 , 3)
+placeToken(player2 , 6)
+
+
+// placeToken(player1 , 7)
+// placeToken(player2 , 7)
+
+
+winGame()
