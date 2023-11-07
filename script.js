d3c10b9f8b47a1e9c089ec26a4bfa0b57411e5ea
Ninja-Jutsu <ninja-jutsu-way@gmail.com>
2023-11-07 11:42:51 +0800

    Setup UI

--- script.js
+++ script.js
@@ -1,92 +1,96 @@
-const board = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-  ]
+function TicTacToe(fName, sName){
+  let board = [
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+    ]
 
-let player1 = {
-  name : "Ismail",
-  token : "X"
-}
+  let player1 = {
+    name : fName,
+    token : "X"
+  }
 
-let player2 = {
-  name : "Vanessa",
-  token : "O"
-}
+  let player2 = {
+    name : sName,
+    token : "O"
+  }
 
-function placeToken(player, position) {
-    for(i = 0; i < board.length ; i++){
-    let tokenPosition = board[i].findIndex(cell => cell === position)
-      if (tokenPosition === -1){
-        continue
+  const placeToken = function (player, position) {
+      for(i = 0; i < board.length ; i++){
+      let tokenPosition = board[i].findIndex(cell => cell === position)
+        if (tokenPosition === -1){
+          continue
+        }
+        else {
+          board[i][tokenPosition] = player.token
+        }
       }
-      else {
-        board[i][tokenPosition] = player.token
-      }
+      console.log(board)
+  }
+
+  const checkWinner = function (){
+    let line = ''
+    //! Loop diagonally from top/left:
+    for (i = 0; i < 3; i++){
+      line += board[i][i]
     }
-}
-
-function winGame(){
-  let text = ''
-
-
-  //! Loop diagonally:
-  for (i = 0; i < 3; i++){
-    text = text + board[i][i]
-  }
-  if (text === "XXX"){
-    return console.log(`${player1.name} Wins`)
-  }
-  else if (text === "OOO"){
-    return console.log(`${player2.name} Wins`)
-  }
-  text = '';
-
-  for (i = 2, j = 0; i >= 0 && j < 3; i--, j++){
-    text = text + board[i][j]
-  } 
-  if (text === "XXX"){
-    return console.log(`${player1.name} Wins`)
-  }
-  else if (text === "OOO"){
-    return console.log(`${player2.name} Wins`)
-  }
-
-  //! Loop through columns
-  for (i = 0; i < 3 ; i ++){
-    const columns = board.map(array => array[i]);
-    text = columns.toString();
-    if (text === "X,X,X"){
+    if (line === "XXX"){
       return console.log(`${player1.name} Wins`)
     }
-    else if (text === "O,O,O"){
+    else if (line === "OOO"){
       return console.log(`${player2.name} Wins`)
     }
-  }
 
-  //! Loop through rows:
-  for (i = 0; i < 3 ; i ++){ 
-    const rows = board[i].map(array => array);
-    text = rows.toString();
-    if (text === "X,X,X"){
+    //! Loop diagonally from top/right:
+    for (i = 2, j = 0; i >= 0 && j < 3; i--, j++){
+      line += board[i][j]
+    } 
+    if (line === "XXX"){
       return console.log(`${player1.name} Wins`)
     }
-    else if (text === "O,O,O"){
+    else if (line === "OOO"){
       return console.log(`${player2.name} Wins`)
     }
+
+    //! Loop through columns
+    for (i = 0; i < 3 ; i++){
+      const columns = board.map(array => array[i]);
+      line = columns.toString();
+      if (line === "X,X,X"){
+        return console.log(`${player1.name} Wins`)
+      }
+      else if (line === "O,O,O"){
+        return console.log(`${player2.name} Wins`)
+      }
+    }
+
+    //! Loop through rows:
+    for (i = 0; i < 3 ; i ++){ 
+      line = board[i].toString();
+      if (line === "X,X,X"){
+        return console.log(`${player1.name} Wins`)
+      }
+      else if (line === "O,O,O"){
+        return console.log(`${player2.name} Wins`)
+      }
+    }
+    line = '';
   }
 
-  text = '';
+  //! Reset the GameBoard:
+  const resetBoard = function(){
+    board = [
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+    ]
+    return console.log(board)
+  }
+  return {player1 , player2 , resetBoard , checkWinner, placeToken}
 }
 
 
-//! Reset the GameBoard:
-function resetBoard(){
-  board = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-  ]
-}
+player = TicTacToe("Ismail", "Vanessa")
+player.checkWinner()
 
-winGame()
+
