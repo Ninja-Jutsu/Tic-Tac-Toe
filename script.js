4bd00c66dc354fe8047a74c0fd0bb3e5061c0877
Ninja-Jutsu <ninja-jutsu-way@gmail.com>
2023-11-08 18:26:10 +0800

    Edit form Style

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function TicTacToe(fName, sName){
 
 
 //+ Create the GameBoard:
+const GameBoard = function makeBoard(){
   const GameBoard = function (){
     let z = 4
     let w = 1
@@ -32,6 +33,7 @@ function TicTacToe(fName, sName){
 
 //+ Play a move:
   const placeToken = function (position) {
+    console.log(board)
       for(i = 0; i < board.length ; i++){
       let tokenPosition = board[i].findIndex(cell => cell === position)
         if (tokenPosition === -1){
@@ -40,9 +42,15 @@ function TicTacToe(fName, sName){
         else {
           board[i][tokenPosition] = actualPlayer.token
           switchPlayer()
+          console.log(actualPlayer)
         }
       }
+      console.log(board)
   }
+
+  placeToken("5")
+  placeToken("7")
+
 
 //+ Check THe winner:
   const checkWinner = function (){
@@ -57,8 +65,7 @@ function TicTacToe(fName, sName){
     else if (line === "OOO"){
       return console.log(`${player2.name} Wins`)
     }
-
-  //* Loop diagonally from top/right:
+    //* Loop diagonally from top/right:
     for (i = 2, j = 0; i >= 0 && j < 3; i--, j++){
       line += board[i][j]
     } 
@@ -67,45 +74,12 @@ function TicTacToe(fName, sName){
     }
     else if (line === "OOO"){
       return console.log(`${player2.name} Wins`)
-    }
-
-  //* Loop through columns
-    for (i = 0; i < 3 ; i++){
-      const columns = board.map(array => array[i]);
-      line = columns.toString();
-      if (line === "X,X,X"){
-        return console.log(`${player1.name} Wins`)
-      }
-      else if (line === "O,O,O"){
-        return console.log(`${player2.name} Wins`)
-      }
-    }
-
-  //* Loop through rows:
-    for (i = 0; i < 3 ; i ++){ 
-      line = board[i].toString();
-      if (line === "X,X,X"){
-        return console.log(`${player1.name} Wins`)
-      }
-      else if (line === "O,O,O"){
-        return console.log(`${player2.name} Wins`)
-      }
-    }
-    line = '';
-  }
-
-  checkWinner()
-  //+ Reset the GameBoard:
-  const resetBoard = function(){
-    board = [
-      [],
-      [],
-      []
-    ]
-    return console.log(board)
-  }
+@git c
 
   function switchPlayer(){
+    if (actualPlayer === "player1")
+    actualPlayer = "player2"
+  }
     if (actualPlayer === player2)
     actualPlayer = player1
     else{actualPlayer = player2}
@@ -118,10 +92,16 @@ function TicTacToe(fName, sName){
     }
   }
 
+  function changeCellContent(){
+
+  }
+
+
 
   return {player1 , player2 , resetBoard , checkWinner, placeToken}
 }
 
+// TicTacToe().placeToken(5)
 
 
 TicTacToe("Is", "Van")
